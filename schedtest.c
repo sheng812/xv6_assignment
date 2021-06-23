629794f57a8c2faf6c5e8ca95b9beea350798172
Hsueh-Sheng Fu <asd074812@gmail.com>
2021-06-23 08:50:12 -0500

    arg success

--- schedtest.c
+++ schedtest.c
@@ -16,23 +16,25 @@ main(int argc, char **argv)
   if (p_a == -1) {
     return -1;
   } else if (p_a == 0) {
-    char* ara[] = {"look", 0};
+    char* ara[] = {"look", "      "};
     strcpy(ara[1], argv[2]);
     // *ara[1] = *argv[2];
-    printf(1, "%d\n", getpid());
+    // printf(1, "%d\n", getpid());
     setslice(getpid(), atoi(argv[1]));
+    // printf(1, "%s\n", argv[2]);
+    // printf(1, "%s\n", ara[1]);
     exec("loop", ara);
   } else {
     p_b = fork();
     if (p_b == -1) {
       return -2;
     } else if (p_b == 0) {
-      char* arb[] = {"loop", 0};
+      char* arb[] = {"loop", "      "};
       strcpy(arb[1], argv[4]);
       // *arb[1] = *argv[4];
-      printf(1, "%d\n", getpid());
+      // printf(1, "%d\n", getpid());
       setslice(getpid(), atoi(argv[3]));
-      printf(1, "%s\n", *arb[1]);
+      // printf(1, "%s\n", *arb[1]);
       exec("loop", arb);
     } else {
       sleep(atoi(argv[5]));
